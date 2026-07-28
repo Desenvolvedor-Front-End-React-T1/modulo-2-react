947effe1c51685964d812ea740281d5f1f34f528
Yan Esteves <yan.m.esteves@gmail.com>
2026-07-28 19:00:42 -0300

    Semana 2 - Aula 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,40 @@ import CardEsporte from './components/CardEsporte'
 function App() {
 
   const tiposNoticia = ["Esporte", "Entretenimento", "Jornalismo"]
+  const noticias = [
+    {
+      id: 1,
+      titulo: 'Brasil x Itália na semi da Liga das Nações',
+      imagem: 'https://s2-home-globo.glbimg.com/mcnN4AOaenqqhb_pn4w_x4XWDa4=/0x46:1280x766/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2026/W/H/VzgiExTOSHtuobrkyM6A/brasilxjapao-vnl-4.jpg',
+      tipo: 'esporte'
+    },
+    {
+      id: 2,
+      titulo: 'Sem Pirlo, Leonardo e Maldini pedem para sair da Federação Italiana',
+      imagem: 'https://s2-home-globo.glbimg.com/VqLrKqfA-W2m9Xf7h3CbvzNF55w=/0x233:3840x2393/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2026/p/K/BLi2knSlqtovOgmXhy1A/cv1-7644-z5gecxvq-20260723025429-jpeg.jpg',
+      tipo: 'esporte'
+    },
+    {
+      id: 3,
+      titulo: 'Entenda nova regra para evitar cera em lesões de goleiros que será testada no futebol inglês',
+      imagem: 'https://s2-home-globo.glbimg.com/PfDHXCrN6jPCgV8DTgWzsAZ1mko=/0x78:2795x1650/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2024/F/O/Mz0SacTuAHLgcrataq1w/gettyimages-2176917933.jpg',
+      tipo: 'esporte'
+    },
+  ]
+  // tiposNoticia.forEach(tipo => console.log(tipo))
 
-  const logado = true
+  const nome = ""
 
-  function fazerLogin() {
-    alert('Houve clique no botao')
+  function clicarBotao() {
+    alert('houve um clique no botão')
   }
-
+  
   return (
     <>
       <h1>App.jsx</h1>
+      <h2>Quantidade de noticias: {noticias.length}</h2>
       
-      <button onClick={fazerLogin}>Clique aqui</button>
-      
-      {logado ? <p>Bem vindo!</p> : <p>Faça login</p>}
+      <input placeholder='Digite seu nome' value={nome} />
 
       <div className='lista-tipos-noticia'>
         {tiposNoticia.map(tipo => (
@@ -25,20 +45,26 @@ function App() {
         ))}
       </div>
 
-      {/* Card Notícia */}
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '14px', marginTop: '12px' }}>
+      <div className='lista-noticias'>
+        {noticias.map(noticia => (
+          <CardEsporte 
+              key={noticia.id}
+              tituloNoticia={noticia.titulo}
+              imagemNoticia={noticia.imagem}
+          />
+        ))}
 
-        {/* Brasil x Itália */}
-        <CardEsporte
+        {/* Uso sem map (lista com laço de repetição para renderização) */}
+        {/* <CardEsporte
           tituloNoticia="Brasil x Itália na semi da Liga das Nações"
           imagemNoticia={"https://s2-home-globo.glbimg.com/mcnN4AOaenqqhb_pn4w_x4XWDa4=/0x46:1280x766/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2026/W/H/VzgiExTOSHtuobrkyM6A/brasilxjapao-vnl-4.jpg"}
-        />
+        /> */}
 
-        <CardEsporte
-          tituloNoticia="Coritiba tem interesse na ..."
-          imagemNoticia={"https://s2-ge.glbimg.com/lLryxWN5YFhCQLeAGEQZgZGfzlU=/810x456/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2026/y/w/qpjD6yR0u3jVyFiXD0Fg/agif26072301462292.jpg"}
-        />
+        {noticias.length === 0 && <p>Não existem notícias a serem exibidas</p>}              
       </div>
+
+
+       <button onClick={clicarBotao}>Clique aqui</button>
 
     </>
   )
